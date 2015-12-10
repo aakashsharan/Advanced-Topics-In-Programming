0645355f10c452fd9475d543d4af162b6878e800
Aakash <noobstylecoding@gmail.com>
2015-12-10 16:54:16 -0600

    initial linked list structure

---
+++ DoublyLinkedList/src/DLinkedList.java
@@ -0,0 +1,22 @@
+import java.util.AbstractList;
+
+public class DLinkedList<E> extends AbstractList<E> {
+
+	LLNode<E> head;
+	LLNode<E> tail;
+	LLNode<E> data;
+	
+	
+	@Override
+	public E get(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
