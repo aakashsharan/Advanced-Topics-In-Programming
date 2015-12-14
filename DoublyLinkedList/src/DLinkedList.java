895438dfe215fcea2207f28ac5083f4e563a4b8b
Aakash <noobstylecoding@gmail.com>
2015-12-14 16:13:37 -0600

    add(int,E) remove and set methods implementation

--- DoublyLinkedList/src/DLinkedList.java
+++ DoublyLinkedList/src/DLinkedList.java
@@ -7,7 +7,9 @@ public class DLinkedList<E> extends AbstractList<E> {
 	int size;
 	
 	
-	// empty linked list, head and tail pointing to each other.
+	/**
+	 * 
+	 */
 	public DLinkedList(){
 		head = new LLNode<E>();
 		tail = new LLNode<E>();
@@ -16,7 +18,11 @@ public class DLinkedList<E> extends AbstractList<E> {
 		size=0;
 	}
 	
-	
+	/** Get the element at the position index.
+	 * @param index the index position from where to retrieve the element.
+	 * @throws IndexOutOfBoundsException if the index if outside the bounds of the list.
+	 * @return data of the specified index.
+	 */
 	public E get(int index) {
 		
 		LLNode<E> currentNode = head;
@@ -41,12 +47,18 @@ public class DLinkedList<E> extends AbstractList<E> {
 		
 	}
 
+	/** Return the size of the list.
+	 * @return size of the list.
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return size;
 	}
 	
+	/**
+	 * @param element The element to be added.
+	 * @return a boolean, true if added else false.
+	 */
 	public boolean add(E element){
 		
 		LLNode<E> llNode = new LLNode<E>(element);
@@ -61,6 +73,119 @@ public class DLinkedList<E> extends AbstractList<E> {
 		
 		return true;
 		
+	}
+	
+	/** Add an element to the list at the specified index
+	 * @param index The index where element needs to be added.
+	 * @param element The element to add.
+	 * @throws NullPointerException if the element is null.
+	 * @throws IndexOutOfBoundsException if the index if outside the bounds of the list.
+	 */
+	public void add(int index, E element){
+		
+		LLNode<E> currentNode = head;
+		LLNode<E> node1 = null;
+		LLNode<E> node2 = null;
+		LLNode<E> addNode = new LLNode<E>(element);
+		
+		if(element == null){
+			throw new NullPointerException("element cannot be nul");
+		}
+		if(index<0 || index<size){
+			throw new IndexOutOfBoundsException("check index");
+		}
+		if(size == 0){
+			
+			addNode.next = tail;
+			addNode.prev = head;
+			head.next = addNode;
+			tail.prev = addNode;
+			size++;
+		}
+		else{
+			currentNode = currentNode.next.next;
+			for(int i=0; i<size; i++){
+				if(i == index){
+					node1 = currentNode;
+					node2 = currentNode.prev;
+				}
+				currentNode = currentNode.next;
+			}
+			
+			addNode.next = node1;
+			addNode.prev = node2;
+			node2.next = addNode;
+			node1.prev = addNode;
+			size++;
+		}
+	}
+	
+ 
+	/** Removes a node at the specified index and return its data element
+	 * @param index The index of the element to remove.
+	 * @return The data element removed
+	 * @throws IndexOutOfBoundsException if index is outside the bounds of the list.
+	 */
+	public E remove(int index){
+		
+		LLNode<E> currentNode = head;
+		LLNode<E> node1 = null;
+		LLNode<E> node2 = null;
+		E data = null;
+		
+		if(index<0 || index>=size){
+			throw new IndexOutOfBoundsException("check index");
+		}
+		else{
+			currentNode = currentNode.next;
+			for(int i=0; i<size; i++){
+				if(i == index){
+					node1 = currentNode;
+					node2 = currentNode.next;
+					data = node1.data;
+				}
+				currentNode = currentNode.next;
+			}
+			
+			LLNode<E> watchNode = node1.prev;
+			watchNode.next = node2;
+			node2.prev = watchNode;
+			size--;
+		}
+		return data;
+	}
+	
+	/** Set an index position in the list to the new element.
+	 * @param index The index of the element to change.
+	 * @param element The new element to be added.
+	 * @throws IndexOutOfBoundsException if the index os outside the bounds of the list.
+	 * @throws NullPointerException if the element is null
+	 * @return The element that was replaced.
+	 */
+	public E set(int index, E element){
+		
+		LLNode<E> currentNode = head;
+		E data = null;
+		E tmp = null;
+		
+		if(index<0 || index>=size){
+			throw new IndexOutOfBoundsException("check index");
+		}
+		if(element == null){
+			throw new NullPointerException("element cannot be null");
+		}
+		else{
+			currentNode = currentNode.next;
+			for(int i=0; i<size; i++){
+				if(i == index){
+					data = currentNode.data;
+					tmp = data;
+					data = element;
+				}
+				currentNode = currentNode.next;
+			}
+			return tmp;
+		}
 	}
 
 }
