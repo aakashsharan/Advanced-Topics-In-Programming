70b75653908eead7789dd45fae4a2212275948c7
Aakash <noobstylecoding@gmail.com>
2015-12-14 15:30:22 -0600

    get and size methods implementation

--- DoublyLinkedList/src/DLinkedList.java
+++ DoublyLinkedList/src/DLinkedList.java
@@ -4,19 +4,63 @@ public class DLinkedList<E> extends AbstractList<E> {
 
 	LLNode<E> head;
 	LLNode<E> tail;
-	LLNode<E> data;
+	int size;
 	
 	
-	@Override
-	public E get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	// empty linked list, head and tail pointing to each other.
+	public DLinkedList(){
+		head = new LLNode<E>();
+		tail = new LLNode<E>();
+		head.next = tail;
+		tail.prev = head;
+		size=0;
+	}
+	
+	
+	public E get(int index) {
+		
+		LLNode<E> currentNode = head;
+		if(index<0 || index>=size){
+			throw new IndexOutOfBoundsException("check index");
+		}
+		if(index == 0){
+			return currentNode.next.data;
+		}
+		else{
+			E data = null;
+			currentNode = currentNode.next.next;
+			for(int i=1; i<size; i++){
+				if(i == index){
+					data = currentNode.data;
+				}
+				currentNode = currentNode.next;
+			}
+			return data;
+		}
+		
+		
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return size;
+	}
+	
+	public boolean add(E element){
+		
+		LLNode<E> llNode = new LLNode<E>(element);
+		LLNode<E> node1 = tail.prev;
+		
+		llNode.next = tail;
+		llNode.prev = node1;
+		
+		node1.next = llNode;
+		tail.prev = llNode;
+		size++;
+		
+		return true;
+		
 	}
 
 }
