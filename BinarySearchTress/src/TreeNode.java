d97ee11249d23037b60f207f99674afeec9b6665
Aakash <noobstylecoding@gmail.com>
2015-12-22 13:02:54 -0600

    added more methods to TreeNode

--- BinarySearchTress/src/TreeNode.java
+++ BinarySearchTress/src/TreeNode.java
@@ -6,11 +6,26 @@ public class TreeNode {
 	private TreeNode left;
 	private TreeNode right;
 	
-	private TreeNode(Comparable k, Object d, TreeNode l, TreeNode r){
+	public TreeNode(Comparable k, Object d, TreeNode l, TreeNode r){
 		key = k;
 		data = d;
 		left = l;
 		right = r;
+	}
+	
+	public TreeNode(Comparable k, TreeNode l, TreeNode r){
+		key = k;
+		left = l;
+		right = r;
+	}
+	
+	
+	public Comparable getKey() {
+		return key;
+	}
+
+	public void setKey(Comparable key) {
+		this.key = key;
 	}
 
 	public Object getData() {
@@ -20,6 +35,24 @@ public class TreeNode {
 	public void setData(Object data) {
 		this.data = data;
 	}
+
+	public TreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+	
+	
 	
 	
 }
